4495bec76b3118abb45fe417d20bc51a915a8669
okwalluis <okwalluis@gmail.com>
2022-07-03 08:00:27 -0400

    Fin de formularios HTML

--- app/config/urls.py
+++ app/config/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # 1 = ruta
     # 2 = funcion que se quiere ejecutar
     path('', views.index, name='index'),
+    path('usuarios/login', views.login_view, name='login'),
+    path('usuarios/logout', views.logout_view, name='logout'),
     path('admin/', admin.site.urls),
 ]
--- app/config/views.py
+++ app/config/views.py
@@ -1,5 +1,13 @@
+from email import message
+import imp
+from django.shortcuts import redirect
 from django.shortcuts import render
-from django.http import HttpResponse
+
+#from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import login #genera la sesion
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate #autentifica el usuario
 
 #Permite responder a una url
 
@@ -7,7 +15,6 @@ def index(request):
     #return HttpResponse('Hola Mundo!')
     return render(request, 'index.html', {
     # context= diccionario
-        'message':'Listado de productos.',
         'title':'Productos',
         'products':[
             {'title':'playera','price':5,'stock':True}, #producto
@@ -15,3 +22,28 @@ def index(request):
             {'title':'Mochila','price':20,'stock':False},
         ]
     })
+
+def login_view(request):
+    #print(request.method)
+    if request.method == 'POST':
+        username = request.POST.get('username') # diccionario
+        password = request.POST.get('password')
+        #imprimir en consola 
+        #print(username)
+        #print(password)
+        user = authenticate(username=username, password=password) #en caso que no encuentre retorna None
+        if user:
+            login(request,user)
+            messages.success(request, "Bienvenido {}". format(user.username))
+            return redirect('index')
+        else:
+            messages.error(request, 'Usuario o contraseña no válidos')
+
+    return render(request, 'users/login.html',{
+
+    })
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Sesión cerrada exitosamente')
+    return redirect('login')
