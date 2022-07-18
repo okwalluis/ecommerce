4a10740454442e290d58983a78f13f2eb288ede3
okwalluis <okwalluis@gmail.com>
2022-07-17 22:25:32 -0400

    Fin de Carrito

--- app/carts/models.py
+++ app/carts/models.py
@@ -1,24 +1,80 @@
 import uuid
+import decimal
+
 from django.db import models
 from users.models import User
 from products.models import Product
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, m2m_changed, post_save
 
 # Create your models here.
 class Cart(models.Model):
     cart_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    subtotal = models.DecimalField(default=0, max_digits=15, decimal_places=0)
-    total = models.DecimalField(default=0, max_digits=15, decimal_places=0)
+    # se crear primero las tablas, luego se crea la relacion
+    #products = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, through='CartProducts')
+    subtotal = models.DecimalField(default=0, max_digits=15, decimal_places=2)
+    total = models.DecimalField(default=0, max_digits=15, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    FEE = 0.05 #5% de utilidad
 
     def __str__(self):
         return self.cart_id
+    
+    def update_totals(self):
+        self.update_subtotal()
+        self.update_total()
+
+    def update_subtotal(self):
+        self.subtotal = sum([
+            cp.quantity * cp.product.price for cp in self.products_related()
+            ])
+        self.save()
+
+    def update_total(self):
+        self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE))
+        self.save()
+
+    #obtenemos todos los objetos cartproducts en una consulta.
+    def products_related(self):
+        return self.cartproducts_set.select_related('product')
+
+class CartProductsManager(models.Manager):
+    def create_or_update_quantity(self, cart, product, quantity=1):
+        object, created = self.get_or_create(cart=cart, product=product)
+        
+        if not created:
+            quantity = object.quantity + quantity
+        
+        object.update_quantity(quantity)
+        return object
+
+class CartProducts(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = CartProductsManager()
+
+    def update_quantity(self, quantity=1):
+        self.quantity = quantity
+        self.save()
 
 def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
         instance.cart_id = str(uuid.uuid4())
 
+#callback
+def update_totals(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.update_totals()
+
+def post_save_update_totals(sender, instance, *args, **kwargs):
+    instance.cart.update_totals()
+
 pre_save.connect(set_cart_id, sender=Cart)
+post_save.connect(post_save_update_totals, sender=CartProducts)
+m2m_changed.connect(update_totals, sender=Cart.products.through)
--- app/carts/views.py
+++ app/carts/views.py
@@ -4,41 +4,34 @@ from .models import Cart
 
 from .utils import get_or_create_cart
 from products.models import Product
+from carts.models import CartProducts
 
 def cart(request):
     cart = get_or_create_cart(request)
-
-#    user = request.user if request.user.is_authenticated else None
-#    cart_id = request.session.get('cart_id') #None
-#    cart = Cart.objects.filter(cart_id=cart_id).first() #()None
-#    if cart is None:
-#        cart = Cart.objects.create(user=user)
-#
-#    if user and cart.user is None:
-#        cart.user = user
-#        cart.save()
-#
-#    request.session['cart_id'] = cart.cart_id
     return render(request, 'carts/cart.html', {
         'cart':cart
     })
 
 def add(request):
     cart = get_or_create_cart(request)
-#    user = request.user if request.user.is_authenticated else None
-#    cart_id = request.session.get('cart_id')
-#    if cart_id:
-#        cart = Cart.objects.get(pk=request.session.get('cart_id'))
-#    else:
-#        cart = Cart.objects.create(user=user)
-
-#    product = Product.objects.get(pk=request.POST.get('product_id'))
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-    #agregar un producto a la lista
-    cart.products.add(product)
+    quantity = int(request.POST.get('quantity', 1))
+    
+    #cart.products.add(product, through_defaults={
+    #    'quantity': quantity
+    #})
+
+    #cart_product = CartProducts.objects.create( cart=cart, 
+    #                                            product=product, 
+    #                                            quantity=quantity)
+    cart_product = CartProducts.objects.create_or_update_quantity(  cart = cart,
+                                                                    product = product,
+                                                                    quantity = quantity)
 
     return render(request, 'carts/add.html', {
-        'product':product
+        'quantity': quantity,
+        'cart_product':cart_product,
+        'product': product
     })
 
 def remove(request):
